543a00d79b8e2a83e9d1d63bf19815baaa46c342
yogaiw <agoy.indra@gmail.com>
2022-10-11 21:35:47 +0700

    few

--- app/src/main/java/com/hureyo/rentalkameramobile/models/Alat.kt
+++ app/src/main/java/com/hureyo/rentalkameramobile/models/Alat.kt
@@ -1,9 +1,9 @@
 package com.hureyo.rentalkameramobile.models
 
 data class Alat(
-    val nama_alat: String?,
+    val nama_alat: String,
     val harga24: Int,
     val harga12: Int,
     val harga6: Int,
-    val nama_kategori: String?
+    val nama_kategori: String
 )
