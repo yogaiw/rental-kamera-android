bb23f7cebfb3e4530501121a8cc91c17a3a3404a
yogaiw <agoy.indra@gmail.com>
2022-10-13 23:39:45 +0700

    Alat filter: response successfull, still with issue
    recyclerview won't refresh

--- app/src/main/java/com/hureyo/rentalkameramobile/Api.kt
+++ app/src/main/java/com/hureyo/rentalkameramobile/Api.kt
@@ -4,10 +4,14 @@ import com.hureyo.rentalkameramobile.models.AlatResponse
 import com.hureyo.rentalkameramobile.models.CategoryResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface Api {
     @GET("alat")
     fun getAlat(): Call<AlatResponse>
+
+    @GET("alat")
+    fun getFilteredAlat(@Query("category") category: Int): Call<AlatResponse>
 
     @GET("category")
     fun getCategory(): Call<CategoryResponse>
--- app/src/main/java/com/hureyo/rentalkameramobile/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/hureyo/rentalkameramobile/adapters/CategoryAdapter.kt
@@ -1,18 +1,49 @@
 package com.hureyo.rentalkameramobile.adapters
 
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.hureyo.rentalkameramobile.R
+import com.hureyo.rentalkameramobile.RetrofitClient
+import com.hureyo.rentalkameramobile.models.Alat
+import com.hureyo.rentalkameramobile.models.AlatResponse
 import com.hureyo.rentalkameramobile.models.Category
 import kotlinx.android.synthetic.main.item_category.view.*
+import retrofit2.Call
+import retrofit2.Response
 
 class CategoryAdapter(private val list: ArrayList<Category>) : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>() {
+    val listAlat = ArrayList<Alat>()
     inner class CategoryViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(categoryResponse: Category) {
             with(itemView) {
                 btn_category.text = categoryResponse.nama_kategori
+                btn_category.setOnClickListener {
+                    RetrofitClient.instance.getFilteredAlat(categoryResponse.id).enqueue(object : retrofit2.Callback<AlatResponse> {
+                        @SuppressLint("NotifyDataSetChanged")
+                        override fun onResponse(
+                            call: Call<AlatResponse>,
+                            response: Response<AlatResponse>
+                        ) {
+                            val listResponse = response.body()?.data
+                            listAlat.clear()
+                            listResponse?.let {
+                                listAlat.addAll(it)
+                            }
+                            val adapter = AlatAdapter(listAlat)
+                            adapter.notifyDataSetChanged()
+                            Toast.makeText(context, listResponse.toString() ,Toast.LENGTH_SHORT).show()
+                        }
+
+                        override fun onFailure(call: Call<AlatResponse>, t: Throwable) {
+
+                        }
+
+                    })
+                }
             }
         }
     }
